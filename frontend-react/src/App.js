13f8243ce80ed84b167bb3c97dac31165b67a747
tuagkcuf <jokersms222@gmail.com>
2023-03-20 20:13:23 +0300

    minor changes

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,11 +3,10 @@ import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { NavBar } from "./components";
 import "./App.scss"
-// TODO: add skills
-// TODO: add projects
 // TODO: add experience
 // TODO: make testimonials and companies
 // TODO: make custom mail
+// TODO: change photo
 const App = () => {
     return (
         <div className="app">
