8521da2f20fa19ad85fe26498eff39f2c011f03b
tuagkcuf <jokersms222@gmail.com>
2023-03-20 20:19:13 +0300

    change header icons

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -34,9 +34,13 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
+import next from "../assets/next.png"
+import web3 from "../assets/web3.png"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+    next,
+    web3,
     email,
     mobile,
     api,
